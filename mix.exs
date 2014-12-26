12a305fea145b3aea8c1b9d0a58c197d36993ce6
Marc Neudert <marc.neudert@gmail.com>
2014-12-26 17:38:42 +0100

    updates dependencies

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Caylir.Mixfile do
       version:       "0.0.1",
       elixir:        "~> 1.0",
       deps:          deps(Mix.env),
-      docs:          [ readme: true, main: "README" ],
+      docs:          [ readme: "README.md", main: "README" ],
       test_coverage: [ tool: ExCoveralls ]]
   end
 
