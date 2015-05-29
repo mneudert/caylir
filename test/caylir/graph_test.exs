54b282e0034470ef5c36c69ee62a1dc4ec2b0747
Marc Neudert <marc.neudert@gmail.com>
2015-05-29 17:20:11 +0200

    cleans up tests

--- test/caylir/graph_test.exs
+++ test/caylir/graph_test.exs
@@ -7,27 +7,26 @@ defmodule Caylir.GraphTest do
     def conf(), do: [ host: "localhost", port: 64210 ]
   end
 
-  setup context do
-    Supervisor.start_link([], name: context.test, strategy: :one_for_one)
-    Supervisor.start_child(context.test, TestGraph.child_spec)
+  setup_all context do
+    Supervisor.start_link([], name: context.case, strategy: :one_for_one)
+    Supervisor.start_child(context.case, TestGraph.child_spec)
 
     :ok
   end
 
-  test "graph startup", context do
-    assert [{ TestGraph, _, _, _ }] = Supervisor.which_children(context.test)
-  end
 
   test "invalid quads fail deleting" do
     { :error, reason } = TestGraph.delete(%{ invalid: "quad" })
 
     assert String.contains?(reason, "invalid quad")
   end
+
   test "invalid quads fail writing" do
     { :error, reason } = TestGraph.write(%{ invalid: "quad" })
 
     assert String.contains?(reason, "invalid quad")
   end
+
 
   test "quad lifecycle", context do
     quad = %{ subject: "quad", predicate: "lifetime", object: context.test }
