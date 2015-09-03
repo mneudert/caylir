0a8a6cf1dae5ddf7fd22d6fa383eaaad5d094a94
Marc Neudert <marc.neudert@gmail.com>
2015-09-03 19:21:15 +0200

    replaces usage of deprecated Behaviour module

--- lib/caylir/graph.ex
+++ lib/caylir/graph.ex
@@ -19,8 +19,6 @@ defmodule Caylir.Graph do
         port: 64210
   """
 
-  use Behaviour
-
   defmacro __using__(otp_app: otp_app) do
     quote do
       alias Caylir.Graph.Config
@@ -52,35 +50,35 @@ defmodule Caylir.Graph do
   @doc """
   Returns the (internal) pool module.
   """
-  defcallback __pool__ :: module
+  @callback __pool__ :: module
 
   @doc """
   Returns a supervisable pool child_spec.
   """
-  defcallback child_spec :: Supervisor.Spec.spec
+  @callback child_spec :: Supervisor.Spec.spec
 
   @doc """
   Returns the connection configuration.
   """
-  defcallback config :: Keyword.t
+  @callback config :: Keyword.t
 
   @doc """
   Deletes a quad from the graph.
   """
-  defcallback delete(Keyword.t) :: t_delete
+  @callback delete(Keyword.t) :: t_delete
 
   @doc """
   Queries the graph.
   """
-  defcallback query(String.t) :: t_query
+  @callback query(String.t) :: t_query
 
   @doc """
   Gets the shape of a query.
   """
-  defcallback shape(String.t) :: t_query
+  @callback shape(String.t) :: t_query
 
   @doc """
   Writes a quad to the graph.
   """
-  defcallback write(Keyword.t) :: t_write
+  @callback write(Keyword.t) :: t_write
 end
