3ebe83d2e8d19ba5a67259091c5d1e05923051c5
Marc Neudert <marc.neudert@gmail.com>
2016-01-08 18:31:48 +0100

    ws / cs

--- lib/caylir/graph/config.ex
+++ lib/caylir/graph/config.ex
@@ -6,13 +6,13 @@ defmodule Caylir.Graph.Config do
   @doc """
   Retrieves the connection configuration for `conn` in `otp_app`.
   """
-  @spec config(otp_app :: atom, conn :: module) :: Keyword.t
+  @spec config(atom, module) :: Keyword.t
   def config(otp_app, conn) do
     if config = Application.get_env(otp_app, conn) do
       [ otp_app: otp_app ] ++ config
     else
-      raise ArgumentError,
-        "configuration for #{ inspect conn } not found in #{ inspect otp_app } configuration"
+      raise ArgumentError, "configuration for #{ inspect conn }" <>
+                           " not found in #{ inspect otp_app } configuration"
     end
   end
 end
