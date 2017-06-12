30ea058573da993e3f832836670446206d43b38e
Marc Neudert <marc.neudert@gmail.com>
2017-06-12 20:42:33 +0200

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Caylir.Mixfile do
 
   defp deps do
     [ { :ex_doc,      ">= 0.0.0", only: :dev },
-      { :excoveralls, "~> 0.6",   only: :test },
+      { :excoveralls, "~> 0.7",   only: :test },
 
       { :hackney, "~> 1.6" },
       { :poison,  "~> 3.0" },
