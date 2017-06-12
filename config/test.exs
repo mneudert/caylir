0a562404b31fb011f641408c8e38e350b780f33a
Marc Neudert <marc.neudert@gmail.com>
2017-06-12 20:44:53 +0200

    ws

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
-config :caylir,
-  Caylir.TestHelpers.Graph,
-    host: "localhost",
-    pool: [ max_overflow: 0, size: 1 ],
-    port: 64210
+config :caylir, Caylir.TestHelpers.Graph,
+  host: "localhost",
+  pool: [ max_overflow: 0, size: 1 ],
+  port: 64210
