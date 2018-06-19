69bfc156e837ef193070d0076d24fd2b421f7991
Marc Neudert <marc.neudert@gmail.com>
2018-06-19 19:41:17 +0200

    cs

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,11 @@ config :caylir, Caylir.TestHelpers.Graphs.DefaultGraph,
   pool: [max_overflow: 0, size: 1],
   port: 64210
 
+config :caylir, Caylir.TestHelpers.Graphs.EnvGraph,
+  host: {:system, "CAYLIR_TEST_ENV_HOST"},
+  pool: [max_overflow: 0, size: 1],
+  port: {:system, "CAYLIR_TEST_ENV_PORT"}
+
 # port will be properly set during test setup
 config :caylir, Caylir.TestHelpers.Graphs.InetsGraph,
   host: "localhost",
@@ -15,8 +20,3 @@ config :caylir, Caylir.TestHelpers.Graphs.InitGraph,
   host: "localhost",
   pool: [max_overflow: 0, size: 1],
   port: 64210
-
-config :caylir, Caylir.TestHelpers.Graphs.EnvGraph,
-  host: {:system, "CAYLIR_TEST_ENV_HOST"},
-  pool: [max_overflow: 0, size: 1],
-  port: {:system, "CAYLIR_TEST_ENV_PORT"}
