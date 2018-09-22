937b0a2321d2f2a0234349c034442a5193ba3492
Marc Neudert <marc.neudert@gmail.com>
2018-09-22 12:43:18 +0200

    minimizes test graph configuration

--- config/test.exs
+++ config/test.exs
@@ -2,22 +2,18 @@ use Mix.Config
 
 config :caylir, Caylir.TestHelpers.Graphs.DefaultGraph,
   host: "localhost",
-  pool: [max_overflow: 0, size: 1],
   port: 64210
 
 config :caylir, Caylir.TestHelpers.Graphs.EnvGraph,
   host: {:system, "CAYLIR_TEST_ENV_HOST"},
-  pool: [max_overflow: 0, size: 1],
   port: {:system, "CAYLIR_TEST_ENV_PORT"}
 
 # port will be properly set during test setup
 config :caylir, Caylir.TestHelpers.Graphs.InetsGraph,
   host: "localhost",
-  pool: [max_overflow: 0, size: 1],
   port: 9999
 
 config :caylir, Caylir.TestHelpers.Graphs.LimitGraph,
   host: "localhost",
   limit: 1,
-  pool: [max_overflow: 0, size: 1],
   port: 64210
