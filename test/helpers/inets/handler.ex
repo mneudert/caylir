b072011af7f89b7bb62f543bc9cd26c8cfeeecda
Marc Neudert <marc.neudert@gmail.com>
2018-09-26 20:04:31 +0200

    cleans up inets fake handler

--- test/helpers/inets/handler.ex
+++ test/helpers/inets/handler.ex
@@ -8,7 +8,6 @@ defmodule Caylir.TestHelpers.Inets.Handler do
   def serve(mod_data), do: serve_uri(mod(mod_data, :request_uri), mod_data)
 
   defp serve_uri('/api/v1/query/gizmo', _mod_data), do: serve_dummy()
-  defp serve_uri('/api/v1/query/gremlin', _mod_data), do: serve_dummy()
 
   defp serve_dummy() do
     body = '{"result": "dummy"}'
