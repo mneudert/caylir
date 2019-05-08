b3fd97cdec5191fe4fb084090aa58edebb56dbe1
Marc Neudert <marc.neudert@gmail.com>
2019-05-08 20:01:33 +0200

    Move inets test helper setup to test case

--- test/caylir/inets_proxy/timeout_test.exs
+++ test/caylir/inets_proxy/timeout_test.exs
@@ -2,6 +2,34 @@ defmodule Caylir.InetsProxy.TimeoutTest do
   use ExUnit.Case, async: true
 
   alias Caylir.TestHelpers.Graphs.InetsGraph
+  alias Caylir.TestHelpers.Inets.Handler
+
+  setup_all do
+    root = String.to_charlist(__DIR__)
+
+    httpd_config = [
+      document_root: root,
+      modules: [Handler],
+      port: 0,
+      server_name: 'caylir_testhelpers_inets_handler',
+      server_root: root
+    ]
+
+    {:ok, httpd_pid} = :inets.start(:httpd, httpd_config)
+
+    inets_env = [
+      host: "localhost",
+      port: :httpd.info(httpd_pid)[:port]
+    ]
+
+    Application.put_env(:caylir, InetsGraph, inets_env)
+
+    {:ok, _} = start_supervised(InetsGraph)
+
+    on_exit(fn ->
+      :inets.stop(:httpd, httpd_pid)
+    end)
+  end
 
   test "timeout" do
     timeout = 10
