a218d5c68b2ed0b88ca025ea0938d33247beb34b
Marc Neudert <marc.neudert@gmail.com>
2022-08-08 16:27:00 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule Caylir.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
       {:hackney, "~> 1.6"},
